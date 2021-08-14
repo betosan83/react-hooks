813729aaee4f3d03d7c446330745ac17bd2fe2d5
Roberto Boscolo <roberto.bfigueredo@dataprev.gov.br>
2021-08-14 19:42:25 -0300

    Use reducer challenge!

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -12,6 +12,14 @@ function reducer(state, action) {
     switch(action.type) {
         case 'numberAdd2':
             return {...state, number: state.number + 2}
+        case 'numberTimes7':
+            return {...state, number: state.number * 7}
+        case 'numberDiv25':
+            return {...state, number: state.number / 25}
+        case 'numberInt':
+            return {...state, number: parseInt(state.number)}
+        case 'numberAddN':
+            return {...state, number: state.number + action.payload}
         case 'login':
             return {...state, user: {name: action.payload}}    
         default:
@@ -35,6 +43,14 @@ const UseReducer = (props) => {
                         onClick={() => dispatch({ type : 'login', payload: 'Roberto' })}>Login</button>
                     <button className="btn"
                         onClick={() => dispatch({ type: 'numberAdd2' })}>+2</button>
+                    <button className="btn"
+                        onClick={() => dispatch({ type: 'numberTimes7' })}>*7</button>
+                    <button className="btn"
+                        onClick={() => dispatch({ type: 'numberDiv25' })}>/25</button>
+                    <button className="btn"
+                        onClick={() => dispatch({ type: 'numberInt' })}>Parse Int</button>
+                    <button className="btn"
+                        onClick={() => dispatch({ type: 'numberAddN', payload: -9 })}>-9</button>
                 </div>
             </div>
         </div>
