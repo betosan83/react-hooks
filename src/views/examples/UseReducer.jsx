21efdea41f941cb25707d2fa027dad6eba75388b
Roberto Boscolo <roberto.bfigueredo@dataprev.gov.br>
2021-08-15 23:27:50 -0300

    Code refactor!

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -1,31 +1,8 @@
 import React, { useReducer } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
-const initialState = {
-    cart: [],
-    products: [],
-    user: null,
-    number: 0
-}
+import {initialState, reducer } from '../../store'
 
-function reducer(state, action) {
-    switch(action.type) {
-        case 'numberAdd2':
-            return {...state, number: state.number + 2}
-        case 'numberTimes7':
-            return {...state, number: state.number * 7}
-        case 'numberDiv25':
-            return {...state, number: state.number / 25}
-        case 'numberInt':
-            return {...state, number: parseInt(state.number)}
-        case 'numberAddN':
-            return {...state, number: state.number + action.payload}
-        case 'login':
-            return {...state, user: {name: action.payload}}    
-        default:
-            return state    
-    }
-}
 
 const UseReducer = (props) => {
     const [state, dispatch] = useReducer(reducer, initialState)
